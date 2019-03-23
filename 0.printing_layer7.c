2ef3d8516f77b43dd784d29a95422637c9fc1f44
YeonGyu Kim <code.yeon.gyu@gmail.com>
2019-03-23 11:10:51 +0900

    Update 0.printing_layer7.c

--- 0.printing_layer7.c
+++ 0.printing_layer7.c
@@ -3,31 +3,30 @@
 int main(void)
 {
 
-	int height, width, i, j, k, l;
+    int height, width, i, j, k, l;
 
-	char line[8][82] = {
+    char line[8][82] = {
 
-		"    *                   *        *       *   *********   *****       **********",
-		"    *                  * *        *     *    *           *    *               *",
-		"    *                 *   *        *   *     *           *     *             *",
-		"    *                *     *        * *      *           *    *             *",
-		"    *               *********        *       *********   *****             *",
-		"    *              *         *       *       *           **               *",
-		"    *             *           *      *       *           * *             *",
-		"    ***********  *             *     *       *********   *  *           *"};
-	printf("Height: ");
-	scanf("%d", &height);
-	printf("Width: ");
-	scanf("%d", &width);
-	for (i = 0; i < 8; i++)
-	{
-		for (l = 0; l < height; l++)
-		{
-			for (j = 0; j < (int)sizeof(line[i]); j++)
-				for (k = 0; k < width; k++)
-					printf("%c", line[i][j]);
-			printf("\n");
-		}
-	}
-	return 0;
+        "    *                   *        *       *   *********   *****       **********",
+        "    *                  * *        *     *    *           *    *               *",
+        "    *                 *   *        *   *     *           *     *             *",
+        "    *                *     *        * *      *           *    *             *",
+        "    *               *********        *       *********   *****             *",
+        "    *              *         *       *       *           **               *",
+        "    *             *           *      *       *           * *             *",
+        "    ***********  *             *     *       *********   *  *           *"
+    };
+    printf("Height: ");
+    scanf("%d", &height);
+    printf("Width: ");
+    scanf("%d", &width);
+    for (i = 0; i < 8; i++) {
+        for (l = 0; l < height; l++) {
+            for (j = 0; j < (int)sizeof(line[i]); j++)
+                for (k = 0; k < width; k++)
+                    printf("%c", line[i][j]);
+            printf("\n");
+        }
+    }
+    return 0;
 }
