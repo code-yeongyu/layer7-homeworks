28fd0bd6adcf846a6f589168b82599e1dc3d7bd5
YeonGyu Kim <code.yeon.gyu@gmail.com>
2019-05-16 14:05:02 +0900

    Update tetris.c

--- personal_prj/tetris/tetris.c
+++ personal_prj/tetris/tetris.c
@@ -1,8 +1,8 @@
 #include <Windows.h>
 #include <conio.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define LEFT_WARD 75
 #define RIGHT_WARD 77
@@ -12,13 +12,12 @@
 #define SPIN_KEY_Z 122
 #define HARD_DROP_SPACE 32
 
-enum direction{
-    NONE,
+enum direction {
     LEFT,
     RIGHT,
     DOWN
 };
-enum blockState{
+enum blockState {
     EMPTY,
     SOFT_BLOCK,
     HARD_BLOCK,
@@ -52,24 +51,23 @@ const int blockShapes[8][4][4][4] = {
         //   ■ ■
         // ■ ■
         0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
+        // a block for nothing(for hold feature)
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void gotoyx(int y, int x)
-{
-    COORD pos = { x, y };
+void gotoyx(int y, int x) {
+    COORD pos = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
 void printMap(enum blockState map[24][12]);
-void createRandomBlock(struct block* b);
+void createRandomBlock(struct block *b);
 void drawPreparingBlock(struct block b);
 void drawHoldingBlock(struct block b);
 void drawBlock(struct block b, enum blockState type);
 void eraseBlock(struct block b);
 int isNowConflict(enum blockState map[24][12], struct block b);
 int willMoveConflict(enum direction way, enum blockState map[24][12], struct block b);
-void moveBlock(enum direction way, struct block* b);
+void moveBlock(enum direction way, struct block *b);
 void putBlockToMap(enum blockState (*map)[12], struct block b);
 void destroyLine(enum blockState (*map)[12], int line);
 void scoreIfAble(enum blockState (*map)[12], int *score, int y);
@@ -77,30 +75,30 @@ void rePrintMapTo(enum blockState (*map)[12], int y);
 void printScore(int score);
 void printStage(int stage);
 
-int main(void){
+int main(void) {
     system("cls");
 
-    int i, j, x, y, timer, score=0, stage=1, scoreForNextLevel=1500, speed=1000, isHold=0, *cacheForRotation;
+    int i, j, x, y, timer, score = 0, stage = 1, scoreForNextLevel = 1500, speed = 1000, isHold = 0, *cacheForRotation;
     char *name, *resultSentence, *path;
 
     struct block preparingBlock, currentBlock, temp, holdingBlock;
     holdingBlock.id = 7;
     holdingBlock.rotationState = 0;
-    enum blockState map[24][12] = { EMPTY };
 
-    //setting up stage
+    enum blockState map[24][12] = {EMPTY};
     for (i = 0; i < 12; i++)
         map[23][i] = WALL;
     for (i = 0; i < 24; i++) {
         map[i][0] = WALL;
         map[i][11] = WALL;
     }
+    // setting up stage
 
     CONSOLE_CURSOR_INFO Curinfo;
     Curinfo.dwSize = 1;
     Curinfo.bVisible = 0;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Curinfo);
-    //removing cursor
+    // removing cursor
 
     srand(time(NULL));
     printMap(map);
@@ -118,23 +116,24 @@ int main(void){
         for (;;) {
             timer = clock();
             while (clock() - timer < speed) {
-                Sleep(1);
+                Sleep(1); //hack for optimization
                 while (kbhit()) {
                     eraseBlock(currentBlock);
                     switch (getch()) {
-                        case LEFT_WARD: //move left
+                        case LEFT_WARD: // move left
+
                             if (!willMoveConflict(LEFT, map, currentBlock))
                                 moveBlock(LEFT, &currentBlock);
                             break;
-                        case RIGHT_WARD: //move right
+                        case RIGHT_WARD: // move right
                             if (!willMoveConflict(RIGHT, map, currentBlock))
                                 moveBlock(RIGHT, &currentBlock);
                             break;
-                        case DOWN_WARD: //move down
+                        case DOWN_WARD: // move down
                             if (!willMoveConflict(DOWN, map, currentBlock))
                                 moveBlock(DOWN, &currentBlock);
                             break;
-                        case UP_WARD: //rotate block
+                        case UP_WARD: // rotate block
                             cacheForRotation = malloc(sizeof(int));
                             *cacheForRotation = currentBlock.rotationState;
                             currentBlock.rotationState++;
@@ -148,7 +147,7 @@ int main(void){
                         case SPIN_KEY_Z:
                             cacheForRotation = malloc(sizeof(int));
                             *cacheForRotation = currentBlock.rotationState;
-                            if(!currentBlock.rotationState)
+                            if (!currentBlock.rotationState)
                                 currentBlock.rotationState = 3;
                             else
                                 currentBlock.rotationState--;
@@ -159,18 +158,19 @@ int main(void){
                             free(cacheForRotation);
                             break;
                         case HOLD_KEY_C:
-                            if(!isHold){
+                            if (!isHold) {
                                 isHold = 1;
                                 temp = holdingBlock;
                                 holdingBlock = currentBlock;
                                 holdingBlock.y = 3;
                                 holdingBlock.x = 4;
-                                if(temp.id == 7){
+                                if (temp.id == 7)
+                                {
                                     drawHoldingBlock(holdingBlock);
                                     eraseBlock(currentBlock);
                                     goto loadNextBlock;
                                 }
-                                currentBlock=temp;
+                                currentBlock = temp;
                                 drawHoldingBlock(holdingBlock);
                                 eraseBlock(currentBlock);
                                 drawBlock(currentBlock, SOFT_BLOCK);
@@ -178,7 +178,7 @@ int main(void){
                             break;
                         case HARD_DROP_SPACE:
                             eraseBlock(currentBlock);
-                            while(!willMoveConflict(DOWN, map, currentBlock))
+                            while (!willMoveConflict(DOWN, map, currentBlock))
                                 moveBlock(DOWN, &currentBlock);
                             drawBlock(currentBlock, HARD_BLOCK);
                             goto loadNextBlockAndFix;
@@ -187,13 +187,13 @@ int main(void){
                     putBlockToMap(map, currentBlock);
                 }
             }
-            if (willMoveConflict(DOWN, map, currentBlock)) { //fixing block
+            if (willMoveConflict(DOWN, map, currentBlock)) { // fixing block
                 loadNextBlockAndFix:
                 for (i = currentBlock.y, y = 0; i < currentBlock.y + 4; i++, y++)
                     for (j = currentBlock.x, x = 0; j < currentBlock.x + 4; j++, x++)
                         if (blockShapes[currentBlock.id][currentBlock.rotationState][y][x])
                             map[i][j] = HARD_BLOCK;
-                //put hard block to map
+                // put hard block to map
 
                 eraseBlock(currentBlock);
                 drawBlock(currentBlock, HARD_BLOCK);
@@ -201,19 +201,19 @@ int main(void){
                 isHold = 0;
                 scoreIfAble(map, &score, currentBlock.y);
 
-
-                for(i = 1; i < 11; i++)
-                    if(map[4][i] == HARD_BLOCK)
+                for (i = 1; i < 11; i++)
+                    if (map[4][i] == HARD_BLOCK)
                         goto gameOver;
-                //check if it's game over
-                if(score >= scoreForNextLevel){ //adding stage
-                    scoreForNextLevel*=2;
+                // check if it's game over
+                if (score >= scoreForNextLevel) { // adding stage
+                    scoreForNextLevel *= 2;
                     printStage(++stage);
-                    speed/=2;
+                    speed /= 2;
                 }
                 printScore(score);
                 break;
-            } else { //moving block down
+            }
+            else { // moving block down
                 eraseBlock(currentBlock);
                 moveBlock(DOWN, &currentBlock);
                 drawBlock(currentBlock, SOFT_BLOCK);
@@ -247,7 +247,7 @@ int main(void){
     return 0;
 }
 
-void printMap(enum blockState (*map)[12]){
+void printMap(enum blockState (*map)[12]) {
     int i, j;
     for (i = 0; i < 24; i++) {
         for (j = 0; j < 12; j++)
@@ -262,13 +262,13 @@ void printMap(enum blockState (*map)[12]){
         puts("");
     }
 }
-void createRandomBlock(struct block* b){
+void createRandomBlock(struct block *b) {
     b->id = rand() % 7;
     b->rotationState = rand() % 4;
     b->y = 3;
     b->x = 4;
 }
-void drawPreparingBlock(struct block b){
+void drawPreparingBlock(struct block b) {
     int i, j;
     gotoyx(0, 22);
     printf("▒▒▒▒▒▒");
@@ -283,7 +283,7 @@ void drawPreparingBlock(struct block b){
     eraseBlock(b);
     drawBlock(b, SOFT_BLOCK);
 }
-void drawHoldingBlock(struct block b){
+void drawHoldingBlock(struct block b) {
     int i, j;
     gotoyx(5, 22);
     printf("▒▒▒▒▒▒");
@@ -298,7 +298,7 @@ void drawHoldingBlock(struct block b){
     eraseBlock(b);
     drawBlock(b, SOFT_BLOCK);
 }
-void drawBlock(struct block b, enum blockState type){
+void drawBlock(struct block b, enum blockState type) {
     int i, j, y, x = 0;
     for (i = b.y, y = 0; i < b.y + 4; i++, y++)
         for (j = b.x * 2, x = 0; j < b.x * 2 + 8; j += 2, x++) {
@@ -315,23 +315,26 @@ void drawBlock(struct block b, enum blockState type){
                 gotoyx(i, j + 2);
         }
 }
-void eraseBlock(struct block b){
+void eraseBlock(struct block b) {
     int i, j, y, x;
     for (i = b.y, y = 0; i < b.y + 4; i++, y++)
         for (j = b.x * 2, x = 0; j < b.x * 2 + 8; j += 2, x++) {
             gotoyx(i, j);
-            blockShapes[b.id][b.rotationState][y][x] == SOFT_BLOCK ? printf("  ") : gotoyx(i, j + 2);
+            blockShapes[b.id][b.rotationState][y][x] == SOFT_BLOCK ? printf("  ")
+                                                                   : gotoyx(i, j + 2);
         }
 }
-int isNowConflict(enum blockState map[24][12], struct block b){
+int isNowConflict(enum blockState map[24][12], struct block b) {
     int i, j, y, x;
     for (i = b.y, y = 0; i < b.y + 4; i++, y++)
         for (j = b.x, x = 0; j < b.x + 4; j++, x++)
-            if (map[i][j] != EMPTY && blockShapes[b.id][b.rotationState][y][x] == SOFT_BLOCK)//if wall and falling block conflicts
+            if (map[i][j] != EMPTY &&
+                blockShapes[b.id][b.rotationState][y][x] ==
+                SOFT_BLOCK) // if wall and falling block conflicts
                 return 1;
     return 0;
 }
-int willMoveConflict(enum direction way, enum blockState map[24][12], struct block b){
+int willMoveConflict(enum direction way, enum blockState map[24][12], struct block b) {
     int i, j, y, x;
     switch (way) {
         case LEFT:
@@ -345,11 +348,11 @@ int willMoveConflict(enum direction way, enum blockState map[24][12], struct blo
     }
     for (i = b.y, y = 0; i < b.y + 4; i++, y++)
         for (j = b.x, x = 0; j < b.x + 4; j++, x++)
-            if (map[i][j] != EMPTY && blockShapes[b.id][b.rotationState][y][x] == SOFT_BLOCK) //if wall and falling block conflicts
+            if (map[i][j] != EMPTY && blockShapes[b.id][b.rotationState][y][x]) // if wall and falling block conflicts
                 return 1;
     return 0;
 }
-void moveBlock(enum direction way, struct block* b){
+void moveBlock(enum direction way, struct block *b) {
     switch (way) {
         case LEFT:
             (b->x)--;
@@ -361,64 +364,64 @@ void moveBlock(enum direction way, struct block* b){
             (b->y)++;
     }
 }
-void putBlockToMap(enum blockState (*map)[12], struct block b){
+void putBlockToMap(enum blockState (*map)[12], struct block b) {
     int i, j, y, x;
     for (i = b.y, y = 0; i < b.y + 4; i++, y++)
         for (j = b.x, x = 0; j < b.x + 4; j++, x++)
             if (blockShapes[b.id][b.rotationState][y][x] && map[i][j] != EMPTY)
                 map[i][j] = blockShapes[b.id][b.rotationState][y][x];
 }
-void destroyLine(enum blockState (*map)[12], int line){
+void destroyLine(enum blockState (*map)[12], int line) {
     int i, j;
-    for(i = line; i != 2;i--)
-        for(j = 1; j <= 10; j++)
-            map[i][j] = map[i-1][j];
+    for (i = line; i != 2; i--)
+        for (j = 1; j <= 10; j++)
+            map[i][j] = map[i - 1][j];
 }
-void scoreIfAble(enum blockState (*map)[12], int *score, int y){
-    int i, j, counter, numOfFullLines=0;
-    for(i = y; i < y+4; i++){
-        for(j = 1, counter=0; j <= 10; j++)
-            if(map[i][j] == HARD_BLOCK)
+void scoreIfAble(enum blockState (*map)[12], int *score, int y) {
+    int i, j, counter, numOfFullLines = 0;
+    for (i = y; i < y + 4; i++) {
+        for (j = 1, counter = 0; j <= 10; j++)
+            if (map[i][j] == HARD_BLOCK)
                 counter++;
-        if(counter == 10){
+        if (counter == 10) {
             destroyLine(map, i);
             rePrintMapTo(map, i);
             numOfFullLines++;
         }
     }
 
-    if(numOfFullLines>3)
-        numOfFullLines+=3;
-    else if(numOfFullLines>2)
-        numOfFullLines+=2;
-    else if(numOfFullLines>1)
-        numOfFullLines+=1;
+    if (numOfFullLines > 3)
+        numOfFullLines += 3;
+    else if (numOfFullLines > 2)
+        numOfFullLines += 2;
+    else if (numOfFullLines > 1)
+        numOfFullLines += 1;
 
-    (*score) += 100*numOfFullLines;
+    (*score) += 100 * numOfFullLines;
 }
-void rePrintMapTo(enum blockState (*map)[12], int y){
+void rePrintMapTo(enum blockState (*map)[12], int y) {
     int i, j, timer;
 
     for (i = 1; i < 11; i++) {
         timer = clock();
         while (clock() - timer < 20);
-        gotoyx(y, i*2);
+        gotoyx(y, i * 2);
         printf("□");
     }
     for (i = 1; i <= y; i++)
         for (j = 1; j < 11; j++) {
-            gotoyx(i, j*2);
+            gotoyx(i, j * 2);
             if (map[i][j] == EMPTY)
                 printf("  ");
             else if (map[i][j] == HARD_BLOCK)
                 printf("■");
         }
 }
-void printScore(int score){
+void printScore(int score) {
     gotoyx(14, 26);
     printf("Score: %d", score);
 }
-void printStage(int stage){
+void printStage(int stage) {
     gotoyx(16, 26);
     printf("Stage: %d", stage);
 }
